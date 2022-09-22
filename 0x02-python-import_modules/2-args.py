1da87e47ccbab75531ab1a040d8c9312136d5179
crysart <idukrystal@gmail.com>
2022-09-22 20:52:48 +0100

    02 f4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     i = 1
     word = "argument" if (l == 2) else "arguments"
     end = "." if (l == 1) else ":"
-    print("{} {}{}".format(l - 1, word, end))
-    for arg in sys.argv:
+    l -= 1
+    print("{} {}{}".format(l, word, end))
+    for arg in sys.argv[1:]:
         print("{}: {}".format(i, arg))
         i += 1
