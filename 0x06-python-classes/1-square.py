ccdd5e2b55791b694a5442f3ac0637b8b3c6a41a
crysart <idukrystal@gmail.com>
2022-10-11 23:22:08 +0100

    01

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""clases demostration task 1"""
+
+
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """init method for square class
+
+        Args:
+            size (int): length of sides
+        """
+
+        self.__size = size
