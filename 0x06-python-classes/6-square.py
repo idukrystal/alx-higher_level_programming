289c184c76834fc86f4b483f8652af527d0437b1
crysart <idukrystal@gmail.com>
2022-10-12 01:11:02 +0100

    06f

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -71,6 +71,6 @@ f 2 positive integers")
 
         print("\n"*self.__position[1], end="")
         for i in range(self.__size):
-            print(("_" * self.__position[0]) + ("#" * self.__size))
+            print((" " * self.__position[0]) + ("#" * self.__size))
         if self.__size == 0:
             print()
