51ee17e7a2a467ec694ffbe729ddc9c725fc240a
crysart <idukrystal@gmail.com>
2022-10-18 00:16:52 +0100

    101

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module to sopve n-queens problem"""
+
+
+def main():
+    """starting point of program"""
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    num = int(sys.argv[1])
+    if num < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
