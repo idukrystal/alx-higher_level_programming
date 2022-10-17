725433ee57f5a4b6c945540607d90554aeb2e4cb
crysart <idukrystal@gmail.com>
2022-10-17 23:16:03 +0100

    00 - 08

---
+++ 0x08-python-more_classes/3-main.py
@@ -0,0 +1,22 @@
+Rectangle = __import__('3-rectangle').Rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle)
+print(repr(my_rectangle))
+
+print("my tests")
+
+rec = Rectangle(4, 0)
+print(rec)
+print("___")
+rec = Rectangle(0, 4)
+print(rec)
