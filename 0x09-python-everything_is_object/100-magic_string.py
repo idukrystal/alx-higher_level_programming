c36a41e91f8dab945a1863c15b033fe3f62ae504
crysart <idukrystal@gmail.com>
2022-10-20 00:10:01 +0100

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,3 @@
-def magic_string():
-    magic_string.counter += 1
-    print(magic_string.counter)
-
-for i in range(10):
-    magic_string()
+def magic_string(vals = []):
+    vals.append("BestSchool")
+    print(", ".join(vals))
