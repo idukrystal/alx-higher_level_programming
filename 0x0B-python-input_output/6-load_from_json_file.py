ed2ebebb30a0f639869c456440a972b1283e2868
crysart <idukrystal@gmail.com>
2022-10-25 22:58:11 +0100

    06

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module to loads json from  a file """
+
+
+def load_from_json_file(filename):
+    """load jason obj ftom filename"""
+    import json
+    with open(filename, 'r', encoding='utf-8') as file:
+        json.load(file)
