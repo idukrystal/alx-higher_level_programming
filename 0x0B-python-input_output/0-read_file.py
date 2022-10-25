af7fc523e2a689530622f2b3a0a75100d5a1893b
crysart <idukrystal@gmail.com>
2022-10-25 22:30:02 +0100

    l00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,8 @@
 
 def read_file(filename=""):
     """outputs contents of filename"""
+    if filename == "":
+        return
     with open(filename, 'r', encoding="utf-8") as fil:
         read_data = fil.read()
         print(read_data)
