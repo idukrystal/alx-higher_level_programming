0452f48aafa8e02c919d87e96939985dc1b94f3e
crysart <idukrystal@gmail.com>
2022-10-26 00:30:38 +0100

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Studentclass module
+"""
+
+
+class Student:
+    """ Student Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dic = self.__dict__
+        if isinstance(attrs, list) & all(isinstance(it, str) for it in attrs):
+            dic = [key for key in dic if key not in attrs]
+        return dic
