3fadd710950241fc42243e3aec38be62f5387462
crysart <idukrystal@gmail.com>
2022-10-25 23:41:02 +0100

    07a

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,13 +2,16 @@
 """apends args to avjdon list file"""
 
 from sys import argv
+from json import JSONDecodeError
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 file = "add_item.json"
+open(file, "w").close()
+
 try:
     obj = load_from_json_file(file)
-except (TypeError):
+except (JSONDecodeError):
     obj = []
-obj.append(list(argv))
+obj.append(list(argv)[1:])
 save_to_json_file(obj, file)
