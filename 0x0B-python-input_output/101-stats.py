2f6887c1049398158a84226d851982195f9d3738
crysart <idukrystal@gmail.com>
2022-10-27 00:12:53 +0100

    101h

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-"""mnbzb bs zbbzbbzbbs hshahha"""
+"""Reads every ten lines of stdin printing errors and storage us\
+ed
+
+Attributes:
+    fs (int): use dto safe total memory used
+    sc (dict): records how many times an err code pops up
+
+"""
