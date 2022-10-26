a7a5c459733fc7a608fbfa9e674f8dc2ca6960a9
crysart <idukrystal@gmail.com>
2022-10-26 01:02:51 +0100

    10zx

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,8 +14,12 @@ class Student:
 
     def to_json(self, attrs=None):
         dic = self.__dict__
-        if isinstance(attrs, list) and
-        all(isinstance(it, str) for it in attrs):
+        if (
+                (isinstance(attrs, list) and
+                 all(isinstance(it, str)
+                     for it in attr)
+                 )
+        ):
             att = [key for key in dic if key not in attrs]
             for key in att:
                 del dic[key]
