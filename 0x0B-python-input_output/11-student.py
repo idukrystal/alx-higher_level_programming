6ae8bbc3dac3801d40dd3a87fbb5de1e113e4580
crysart <idukrystal@gmail.com>
2022-10-26 01:15:12 +0100

    11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,8 +14,15 @@ class Student:
 
     def to_json(self, attrs=None):
         dic = self.__dict__
-        if isinstance(attrs, list) & all(isinstance(it, str) for it in attrs):
+        if (
+                isinstance(attrs, list) and
+                all(isinstance(it, str) for it in attrs)
+        ):
             att = [key for key in dic if key not in attrs]
             for key in att:
                 del dic[key]
         return dic
+
+    def reload_from_json(self, json):
+        for key in json:
+            self.__dict__[key] = json[key]
