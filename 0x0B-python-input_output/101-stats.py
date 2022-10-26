a93720a3a58733eb08e94f4d85c44fdb574a928f
crysart <idukrystal@gmail.com>
2022-10-27 00:25:19 +0100

    101m

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,17 +10,3 @@ Attributes:
 from sys import stdin
 """ imports stdin """
 
-while True:
-""" a while loop """
-    for i in range(10):
-        try:
-            line = stdin.readline()
-            fs += int(getdata(line, 4))
-            sc[int(getdata(line, 3))] += 1
-        except (KeyboardInterrupt):
-            break
-    print(f"File size: {fs}")
-    for key in sc:
-        if sc[key] != 0:
-            print(f"{key}: {sc[key]}")
-            sc[key] = 0
