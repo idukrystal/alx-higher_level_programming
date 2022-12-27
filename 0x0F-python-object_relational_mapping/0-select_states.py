96483e9188b264bc812680ac6addcd44bc0566f5
crysart <idukrystal@gmail.com>
2022-12-27 16:04:38 +0100

    h2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,12 +12,10 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    [print(state) for state in rows]
