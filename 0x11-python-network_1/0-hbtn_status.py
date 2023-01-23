39f4a0302174441e06269b8bbd6673b5d726fd7b
crysart <idukrystal@gmail.com>
2023-01-23 01:51:57 +0100

    gop

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("    - type: {}".format(type(content)))
+        print("    - content: {}".format(content))
+        print("    - utf8 content: {}".format(content.decode('utf-8')))
