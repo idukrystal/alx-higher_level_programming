1b1fd61f9f81ed055012ff29cd7b01b20ed9e8df
crysart <idukrystal@gmail.com>
2023-01-23 02:42:43 +0100

    to

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status
-using  the package requests."""
-
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
